14f7d9b8d38cdeb1d0d0aff31286fc57f7e92d3f
AKKHILESHRP <akkhileshrp2003@gmail.com>
2024-07-05 22:46:10 +0530

    data handled

--- server/index.js
+++ server/index.js
@@ -11,14 +11,17 @@ app.use(cors());
 const configuration = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 const modelId = "gemini-pro";
 const model = configuration.getGenerativeModel({ model: modelId });
-const history = [];
+const userPromptHistory = [];
+const userPromptDataHistory = [];
+
 const generateResponse = async (req, res) => {
     try {
         const { prompt } = req.body;
         const result = await model.generateContent(prompt);
         const response = result.response;
         const text = response.text();
-        history.push(text);
+        userPromptHistory.push(prompt);
+        userPromptDataHistory.push(text);
         res.send(text.toString());
     } catch (error) {
         console.error("Error generating response:", error);
@@ -28,8 +31,11 @@ const generateResponse = async (req, res) => {
 app.get("/", (req, res) => {
     res.send({ message: "Hello, World!" });
 });
-app.get("/history", async (req, res) => {
-    res.send(history);
+app.get("/prompt-history", async (req, res) => {
+    res.send(userPromptHistory);
+});
+app.get("/prompt-data", async (req, res) => {
+    res.send(userPromptDataHistory);
 });
 app.post("/generate", generateResponse);
 app.listen(PORT, () => {
